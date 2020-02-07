11e589d5388227e9370816c4f7f3b0db4200e3b3
J. Cameron McDonald <cameron.mcdonald2@asurion.com>
2020-02-07 10:29:36 -0600

    test: dummy e2e test results

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -1,1 +1,42 @@
-echo e2e
+#!/bin/bash
+
+BASE_DIR=$(dirname "$0")
+CUCUMBER_REPORTS_DIR=$BASE_DIR/../cucumber-reports
+
+mkdir -p $CUCUMBER_REPORTS_DIR
+cat > $CUCUMBER_REPORTS_DIR/empty-report.json << EOF
+[
+  {
+    "id": "a-fake-feature",
+    "uri": "features/fake.feature",
+    "keyword": "Feature",
+    "name": "A Fake feature",
+    "line": 1,
+    "description": "",
+    "elements": [
+      {
+        "keyword": "Scenario",
+        "id": "one-passing-scenario,-one-failing-scenario;passing",
+        "name": "Passing",
+        "line": 5,
+        "description": "",
+        "type": "scenario",
+        "steps": [
+          {
+            "keyword": "Given ",
+            "name": "this step passes",
+            "line": 6,
+            "match": {
+              "location": "features/step_definitions/steps.rb:1"
+            },
+            "result": {
+              "status": "passed",
+              "duration": 1
+            }
+          }
+        ]
+      }
+    ]
+  }
+]
+EOF
